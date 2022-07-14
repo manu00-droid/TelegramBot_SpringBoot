1e493f9335c62dd310f752cfa2b770be24206cd4
manu00-droid <manavsidana00@gmail.com>
2022-07-14 22:52:14 +0530

    translate service edited

--- PythonTranslateService/translate/apis/views.py
+++ PythonTranslateService/translate/apis/views.py
@@ -6,8 +6,8 @@ import translator
 
 @api_view(['POST'])
 def translate(request):
-    data = json.loads(request.body.decode('utf-8'))
-    text = data['text']
-    language = data['language']
+    print(request.data)
+    text = request.data['text']
+    language = request.data['language']
     translated_text = translator.translation(text, language)
     return Response(translated_text)
