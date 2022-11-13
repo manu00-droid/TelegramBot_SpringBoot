fa471e106991d5987e824fd67f5c5c9efd20d4e2
manu00-droid <manavsidana00@gmail.com>
2022-11-13 14:01:20 +0530

    feat:added a endpoint for mandi rates

--- PythonClassifierService/classifiers/apis/urls.py
+++ PythonClassifierService/classifiers/apis/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('prediction/', views.predict)
+    path('prediction/', views.predict),
+    path('mandiRate', views.mandi_rate)
 ]
