f6943094ce78497cadb282552d80538a7226600c
manu00-droid <manavsidana00@gmail.com>
2022-11-15 21:12:12 +0530

    docs:adding commets

--- Bot Service/src/main/java/com/thapar/CropSaviour/Service/ClassifierService.java
+++ Bot Service/src/main/java/com/thapar/CropSaviour/Service/ClassifierService.java
@@ -10,21 +10,26 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class ClassifierService {
+    //base endpoint where we have to send requests
     private final String BASE_URI = "http://127.0.0.1:8083/prediction/";
+    //creating webclient object
     private WebClient webClient = WebClient.create(BASE_URI);
 
     public String classifyDisease(String filePath, String cropType) {
+        //Creating a map for sending form data to the classifier service
         MultiValueMap<String, String> bodyValues = new LinkedMultiValueMap<>();
         bodyValues.add("file_path", filePath);
         bodyValues.add("crop", cropType);
 
+        //Sending a post request
         String disease = webClient.post()
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .body(BodyInserters.fromFormData(bodyValues))
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
-        //remove extra double colon
+        //webclient adds extra double colon in the string
+        //so removing the extra double colon
         disease = disease.substring(1, disease.length() - 1);
         return disease;
     }
